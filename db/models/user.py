91c06e19a1808fda1afc0afa81a08de23a4e94c7
David Becerra <davidnbr_98@hotmail.com>
2024-04-02 20:12:03 -0500

    Delete db directory

--- db/models/user.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel # Libreria pydantic permite gestionar modelos
-
-# str | None: Indica que puede ser un entero o un nulo, es decir, opcional puede que no nos llegue
-
-class User(BaseModel):
-    id: str = None
-    username: str
-    email: str
